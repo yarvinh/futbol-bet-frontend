6f1cc7a75aef131978155152c111f87e45db1992
Yarvin Hernandez <yarvinhhernandez@gmail.com>
2024-09-09 17:16:32 -0400

    new url to fetch the data in production

--- src/helpers/urlAndPaths.js
+++ src/helpers/urlAndPaths.js
@@ -1,6 +1,6 @@
 export const baseUrl = () => { 
     return  process.env.NODE_ENV === "development" ? 'http://localhost:3000'
     : process.env.NODE_ENV === 'test' ? 'http://localhost:3000'
-    : 'https://nourlyet.com'  
+    : 'https://futbol-bet-backend-62bcfc4f5542.herokuapp.com'  
      
  }
